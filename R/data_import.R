8e26d8759dd65ba21632417be299a6277fbf6477
melottidaniele <danielemelotti@hotmail.com>
2023-03-29 23:13:54 +0800

    improved scripts content arrangement

--- R/data_import.R
+++ R/data_import.R
@@ -3,11 +3,12 @@
 # Dataset available at: https://github.com/danielemelotti/decision_tree_exploration
 
 ## Installing the necessary packages:
-install.packages("rpart")
-install.packages("rpart.plot")
-install.packages("rattle")
-install.packages("randomForest")
-install.packages("ranger")
+
+#install.packages("rpart")
+#install.packages("rpart.plot")
+#install.packages("rattle")
+#install.packages("randomForest")
+#install.packages("ranger")
 require(rpart)
 require(rpart.plot)
 require(rattle)
@@ -27,3 +28,17 @@ fulldata <- download_data %>%
   select(-"PARCELNO")
 
 str(fulldata)
+
+dv <- "SALE_PRC" # outcome variable
+model_formula <- SALE_PRC ~ LND_SQFOOT + TOT_LVG_AREA + SPEC_FEAT_VAL + RAIL_DIST + OCEAN_DIST +
+  WATER_DIST + CNTR_DIST + SUBCNTR_DI + HWY_DIST + age + avno60plus + structure_quality
+
+table(is.na(fulldata)) # no missing values
+
+# Performing a 75:25 split
+set.seed(2012)
+train_indices <- sample(1:nrow(fulldata), size = 0.75 * nrow(fulldata))
+
+# Creating the train and test sets
+train_set <- fulldata[train_indices, ]
+test_set <- fulldata[-train_indices, ]
