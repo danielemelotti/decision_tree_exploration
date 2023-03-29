adfaf5ae880220306e2c4ae94c55c7bb3ff9d86a
melottidaniele <danielemelotti@hotmail.com>
2023-03-29 23:13:54 +0800

    extract predictors from  model_formula

--- R/data_import.R
+++ R/data_import.R
@@ -9,12 +9,14 @@
 #install.packages("rattle")
 #install.packages("randomForest")
 #install.packages("ranger")
+#install.packages("insight")
 require(rpart)
 require(rpart.plot)
 require(rattle)
 require(dplyr)
 require(randomForest)
 require(ranger)
+require(insight)
 
 # For the purpose of this exercise, we'll be using a dataset regarding housing prices in Miami.
 # The dataset includes a total of 17 variables.
@@ -29,9 +31,14 @@ fulldata <- download_data %>%
 
 str(fulldata)
 
+head(fulldata)
+
 dv <- "SALE_PRC" # outcome variable
-model_formula <- SALE_PRC ~ LND_SQFOOT + TOT_LVG_AREA + SPEC_FEAT_VAL + RAIL_DIST + OCEAN_DIST +
-  WATER_DIST + CNTR_DIST + SUBCNTR_DI + HWY_DIST + age + avno60plus + structure_quality
+model_formula <- SALE_PRC ~ LATITUDE + LONGITUDE + LND_SQFOOT + TOT_LVG_AREA + SPEC_FEAT_VAL + RAIL_DIST + OCEAN_DIST + WATER_DIST + CNTR_DIST + SUBCNTR_DI + HWY_DIST + age + avno60plus + month_sold + structure_quality
+
+xv_list <- find_predictors(model_formula) # list of predictors
+xv_form <- formula(model_formula)[-2] # predictors as formula (but with tilde)
+xv_call <- formula.tools::rhs(model_formula) # predictors call
 
 table(is.na(fulldata)) # no missing values
 
